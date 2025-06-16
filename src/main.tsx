65aa65b2fb9fdd7edeaea9f580610a88486b584e
Sangoda <87961379+Sangoda@users.noreply.github.com>
2025-06-16 02:52:51 -0700

    Domain Fix?

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,5 @@ import './index.css';
 import "bootstrap/dist/css/bootstrap.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <App />
 );
