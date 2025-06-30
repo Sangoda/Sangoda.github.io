d63e3a5ab77d812a5e6f163cc75506fe89d8ac21
Sangoda <87961379+Sangoda@users.noreply.github.com>
2025-06-29 17:52:18 -0700

    banner

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,17 +1,36 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./Banner.css";
 
 const Banner: React.FC = () => {
+ 
   return (
+
     <div className="banner">
+
+      
       <div className="banner-content">
-        <img src="/RedLogo.png" alt="Logo" className="banner-logo" />
-        <div className="banner-text">
-          <h1>Sanya</h1>
-        <h1>Gowda<span className="blinking-cursor"> |</span></h1> {/* Blinking cursor after Gowda */}
-        </div>
-      </div>
+
+        
+<div className="logo-banner">
+  <span className="logo-part sanya">S</span>
+  <span className="logo-part sanya a1">A</span>
+  <span className="logo-part sanya">N</span>
+  <span className="logo-part sanya y">Y</span>
+  <span className="logo-part sanya a2">A</span> {/* ← second A */}
+  <span className="logo-part gowda">G</span>
+  <span className="logo-part gowda">o</span>
+  <span className="logo-part gowda">w</span>
+  <span className="logo-part gowda">d</span>
+  <span className="logo-part gowda">a</span>
+</div>
+
+
+</div>
+
+<div className='rectangle'> “Locals and Tourists #3 (GTWA #4): San Francisco” by Erica Fischer, CC BY-SA 2.0 </div>
     </div>
+
+    
   );
 };
 
